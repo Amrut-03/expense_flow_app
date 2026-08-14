a4992b3d2c2d8b7c841d651702a73bc0d7d84380
Amrut-03 <amrutkhochikar@gmail.com>
2026-08-14 22:35:59 +0530

    build.gradle updated

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,33 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    val alignKotlinToJava = {
+        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+            val variant = name.removePrefix("compile").removeSuffix("Kotlin")
+            val javaTarget = (tasks.findByName("compile${variant}JavaWithJavac") as? JavaCompile)
+                ?.targetCompatibility?.trim()
+            val jvm = when (javaTarget) {
+                "17" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+                "11" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+                "1.8", "8" -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_1_8
+                else -> org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+            }
+            compilerOptions {
+                jvmTarget.set(jvm)
+            }
+        }
+    }
+
+    if (project.state.executed) {
+        alignKotlinToJava()
+    } else {
+        afterEvaluate {
+            alignKotlinToJava()
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
